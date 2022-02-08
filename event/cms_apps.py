106e0eb6e32e5be5995518e2d740bb5d9bc5f0a4
Aurore Mariscal <auroremariscal@MBP-de-Aurore.lan>
2022-02-08 12:58:05 +0100

    Fix Agenda apphooks

---
+++ event/cms_apps.py
@@ -0,0 +1,13 @@
+from cms.app_base import CMSApp
+from cms.apphook_pool import apphook_pool
+
+
+class AgendaApphook(CMSApp):
+    app_name = "events"
+    name = "Agenda Application"
+
+    def get_urls(self, page=None, language=None, **kwargs):
+        return ["apps.agenda.event.urls"]
+
+
+apphook_pool.register(AgendaApphook)  # register your app
